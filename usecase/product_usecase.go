6f6aba662f5e534b83aef879caf4eaabfd8fbcff
emanuelamaral <amoz_c.p.t@hotmail.com>
2024-06-25 19:05:11 -0300

    First commit

---
+++ usecase/product_usecase.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"example/hello/model"
+	"example/hello/repository"
+)
+
+type ProductUseCase struct {
+	repository repository.ProductRepository
+}
+
+func NewProductUseCase(repo repository.ProductRepository) ProductUseCase {
+	return ProductUseCase{
+		repository: repo,
+	}
+}
+
+func (pu *ProductUseCase) GetProducts() ([]model.Product, error) {
+	return pu.repository.GetProducts()
+}
+
+func (pu *ProductUseCase) CreateProduct(product model.Product) (model.Product, error) {
+
+	productId, err := pu.repository.CreateProduct(product)
+
+	if err != nil {
+		return model.Product{}, err
+	}
+
+	product.ID = productId
+
+	return product, nil
+}
+
+func (pu *ProductUseCase) GetProductById(id_product int) (*model.Product, error) {
+
+	product, err := pu.repository.GetProductById(id_product)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
